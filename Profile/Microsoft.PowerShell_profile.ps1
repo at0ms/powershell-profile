5663726dca3d3ea0672d5a4ceaba07a64ea19a91
at0ms <153468047+at0ms@users.noreply.github.com>
2026-03-09 17:29:18 +0000

    Added missing network function

--- Profile/Microsoft.PowerShell_profile.ps1
+++ Profile/Microsoft.PowerShell_profile.ps1
@@ -37,6 +37,18 @@ function Load-JsonConfig {
     return ($raw | ConvertFrom-Json)
 }
 
+function Test-GitHubConnection {
+    if ($PSVersionTable.PSEdition -eq "Core") {
+        # If PowerShell Core, use a 1 second timeout.
+        return Test-Connection github.com -Count 1 -Quiet -TimeoutSeconds 1
+    } else {
+        # For PowerShell Desktop, use .NET Ping class with timeout.
+        $ping = New-Object System.Net.NetworkInformation.Ping
+        $result = $ping.Send("github.com", 1000) # 1 second timeout.
+        return ($result.Status -eq "Success")
+    }
+}
+
 function Get-ProfileDir {
     if ($PSVersionTable.PSEdition -eq "Core") {
         return [Environment]::GetFolderPath("MyDocuments") + "\PowerShell"
